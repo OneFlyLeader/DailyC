f171bb07f980a7dd95ff8383c17f70eee2700610
OneFlyLeader <67882396+OneFlyLeader@users.noreply.github.com>
2020-10-21 09:11:18 +0800

    初始化

---
+++ cmake-build-debug/CMakeFiles/DailyC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DailyC.dir/system/stack.c.obj"
+  "DailyC.exe"
+  "DailyC.exe.manifest"
+  "DailyC.pdb"
+  "libDailyC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DailyC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ system/stack.c
@@ -0,0 +1,10 @@
+//
+// Created by OneFl on 2020/10/21.
+//
+
+#include <stdio.h>
+
+int main(){
+    printf("this is a stack file\n");
+    return 0;
+}
